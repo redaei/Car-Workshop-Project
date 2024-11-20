470d2de31d444cf96786655216e2695fa7fdc07e
redaei <redaeis@gmail.com>
2024-11-20 20:37:06 +0300

    user1

---
+++ models/bill.js
@@ -0,0 +1,38 @@
+const { text } = require('express')
+const mongoose = require('mongoose')
+
+const billSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Double,
+      required: true
+    },
+    paymentStatus: {
+      type: String,
+      enum: ['Pending', 'Paid'],
+      default: 'Pending'
+    },
+    paymentDate: {
+      type: Date
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    service: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Service'
+    },
+    car: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Car'
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const Bill = mongoose.model('Bill', billSchema)
+
+module.exports = Bill
